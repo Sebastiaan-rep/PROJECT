663308b1fe4233853af4751a0793e4900f787b74
Jiawei <j.w.wang@st.hanze.nl>
2020-01-13 13:58:51 +0100

    test 4

--- Game.java
+++ Game.java
@@ -81,21 +81,6 @@ public class Game
         System.out.println("World of Zuul is a new, incredibly boring adventure game.");
         System.out.println("Type 'help' if you need help.");
         System.out.println();
-        System.out.println("You are " + currentRoom.getDescription());
-        System.out.print("Exits: ");
-        if(currentRoom.northExit != null) {
-            System.out.print("north ");
-        }
-        if(currentRoom.eastExit != null) {
-            System.out.print("east ");
-        }
-        if(currentRoom.southExit != null) {
-            System.out.print("south ");
-        }
-        if(currentRoom.westExit != null) {
-            System.out.print("west ");
-        }
-        System.out.println();
     }
 
     /**
@@ -125,6 +110,7 @@ public class Game
 
         return wantToQuit;
     }
+    
 
     // implementations of user commands:
 
