e8a4a797a5abd33175784f3aede6900f0a13e6e9
Sebastiaan <sebastiaan-rep@hotmail.com>
2020-01-14 13:22:51 +0100

    Aanpassing in code na opdracht 8.7

--- Game.java
+++ Game.java
@@ -82,19 +82,7 @@ public class Game
         System.out.println("Type 'help' if you need help.");
         System.out.println();
         System.out.println("You are " + currentRoom.getDescription());
-        System.out.print("Exits: ");
-        if(currentRoom.northExit != null) {
-            System.out.print("north ");
-        }
-        if(currentRoom.eastExit != null) {
-            System.out.print("east ");
-        }
-        if(currentRoom.southExit != null) {
-            System.out.print("south ");
-        }
-        if(currentRoom.westExit != null) {
-            System.out.print("west ");
-        }
+        System.out.print(currentRoom.getExitString());
         System.out.println();
     }
 
