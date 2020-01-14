29aee1067fabd37b733ce5ae32ca4d6e2e98119b
Jiawei <j.w.wang@st.hanze.nl>
2020-01-14 14:37:27 +0100

    test1

---
+++ test1.java
@@ -0,0 +1,33 @@
+
+/**
+ * class test1 - geef hier een beschrijving van deze class
+ *
+ * @author (jouw naam)
+ * @version (versie nummer of datum)
+ */
+public class test1
+{
+    // instance variables - vervang deze door jouw variabelen
+    private int x;
+
+    /**
+     * Constructor voor objects van class test1
+     */
+    public test1()
+    {
+        // geef de instance variables een beginwaarde
+        x = 0;
+    }
+
+    /**
+     * Voorbeeld van een method - schrijf hier jouw comment
+     *
+     * @param  y    deze method krijgt deze parameter mee in de aanroep
+     * @return    deze method geeft de som van x en y terug
+     */
+    public int voorbeeldMethod(int y)
+    {
+        // schrijf hier jouw code
+        return x + y;
+    }
+}
